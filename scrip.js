047482545e101f8971d8cfb2f96383a38899e116
Daisi <dulceydaisi@gmail.com>
2026-09-13 23:02:08 -0300

    corregido numero de whatsApp

--- scrip.js
+++ scrip.js
@@ -1,5 +1,5 @@
 // REEMPLAZAR CON EL NÚMERO DE FAST BURGER (con código de país, ej: 5491112345678)
-const TELEFONO_BURGER = "5492612712021"; 
+const TELEFONO_BURGER = "5492612470256"; 
 
 let carrito = [];
 
